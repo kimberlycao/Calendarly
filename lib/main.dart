f902341fa371863324155d62e3b7db3fa2ba71d5
kimberlycao <kimberly.cao13@gmail.com>
2020-09-06 18:23:52 -0400

    Firebase set up for Android

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'tf is this',
       theme: ThemeData(
         primarySwatch: colorCustom,
